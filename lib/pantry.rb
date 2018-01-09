3207c47300331d02f84035846d110ca7fa08ec79
smunozjr1986 <sergiomunozjr07@gmail.com>
2018-01-09 09:45:46 -0700

    initial commit

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,15 @@
 class Pantry
 
+  def initialize
+    @stock = {}
+  end
+
+  def stock(item)
+    @stock
+  end
+
+  def stock_cheese(item)
+  end 
+
+
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,35 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_if_instance_pantry_exist
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_checking_adding_stock
+    pantry = Pantry.new
+    result = pantry.stock({})
+
+    assert_equal ({}), result
+  end
+
+  def test_for_cheese
+    pantry = Pantry.new
+    result = pantry.stock_cheese("Cheese")
+
+    assert_equal 0
+  end
+
+  def restock_cheese
+    skip
+    pantry = Pantry.new
+    result = pantry.restock("Cheese", 10)
+    pantry.stock_check("Cheese")
+
+    assert_equal 10
+  end
+
+
+
 end
